b0fd6e5bf34611cd5d28c7611497a005a2d4daed
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2024-03-25 11:05:08 +0300

    add button on countries & govermorates pages

--- src/views/CountriesView.vue
+++ src/views/CountriesView.vue
@@ -7,11 +7,11 @@ const countries = ref([
 	{'id': '2', 'name' : '2asdf'},
 	{'id': '3', 'name' : 'sadf3'},
 ]);
-const submit = (id) => {
+
+const submitDelete = (id) => {
 	axios
-    .get("/countries")
+    .delete("/countries")
     .then((res) => {
-      countries.value = res["data"];
       console.log(res);
     })
     .catch((error) => {
@@ -35,6 +35,13 @@ onMounted(() => {
 <template>
   <article>
     <div class="relative overflow-x-auto md:mx-5">
+      <div class="flex justify-end px-4 pb-4">
+        <button class="focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center text-gray-300 bg-gray-500 hover:bg-gray-600 focus:ring-gray-700">
+          <RouterLink :to="{ name: 'Countries.Create' }">
+            Add new country
+          </RouterLink>
+        </button>
+      </div>
       <table class="w-full text-sm text-left text-gray-400">
         <thead class="text-xs uppercase bg-gray-700 text-gray-400">
           <tr>
@@ -46,7 +53,7 @@ onMounted(() => {
           <tr v-for="country in countries" :key="country.id" class="border-b bg-gray-800 border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-white">{{ country.name }}</th>
             <td class="px-6 py-4">
-					    <form @submit.prevent="submit(country.id)" class="max-w-sm mx-auto">
+					    <form @submit.prevent="submitDelete(country.id)" class="max-w-sm mx-auto">
 								<button type="submit" class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-red-700 hover:bg-red-800 focus:ring-red-900">Delete</button>
 							</form>
 						</td>
